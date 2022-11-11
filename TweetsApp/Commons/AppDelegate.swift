263175e0e07a1ac1758371bd8515f4502a48a38b
Teodoro <teo.calle.lara@gmail.com>
2022-11-11 15:48:03 -0500

    Se crean interfaces principales

---
+++ TweetsApp/Commons/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TweetsApp
+//
+//  Created by TEO on 11/11/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
